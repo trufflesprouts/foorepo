9a73517009515f339de5996501abc9b86951b6cd
Abe Baali <milkyabdullah@gmail.com>
2017-09-10 09:11:15 +0300

    new file

---
+++ app.js
@@ -0,0 +1,25 @@
+---
+layout: default
+---
+
+<ul class="post-list">
+  {% for post in site.posts %}
+    <li class="post">
+      <h2>
+        <a class="post-link" href="{{ post.url | relative_url }}">{{ post.title | escape }}</a>
+      </h2>
+      <div class="text-area">
+        {% if post.content contains '<!--more-->' %}
+         <p>{{ post.content | split:'<!--more-->' | first }}</p>
+        {% else %}
+         <p>{{ post.excerpt }}</p>
+        {% endif %}
+      </div>
+      <p class="post-meta">{{ post.date | date: "%b %-d, %Y" }}</p>
+    </li>
+  {% endfor %}
+
+  {% include newsletter.html %}
+</ul>
+
+<!-- <p class="rss-subscribe">subscribe <a href="{{ "/feed.xml" | relative_url }}">via RSS</a></p> -->
